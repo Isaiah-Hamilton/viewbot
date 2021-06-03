bb6e9abd03b95ea1bcbc997d3f2cf58b0e68a266
Isaiah Hamilton <70828596+Isaiah-Hamilton@users.noreply.github.com>
2021-06-02 20:48:50 -0400

    Update viewbot.py

--- viewbot.py
+++ viewbot.py
@@ -1,4 +1,3 @@
-# Made by Isaiah Hamilton 3-6-2021
 from selenium import webdriver
 from random import randrange
 import time
@@ -8,17 +7,8 @@ url = "YOUTUBE URL"
 refresh_time = 10
 browser_list = []
 
-b1 = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
-b2 = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
-b3 = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
-b4 = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
-b5 = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
-
-browser_list.append(b1)
-browser_list.append(b2)
-browser_list.append(b3)
-browser_list.append(b4)
-browser_list.append(b5)
+chrome = webdriver.Chrome("THE LOCATION OF CHROMEDRIVER")
+browser_list.append(chrome)
 
 for browser in browser_list:
     browser.get(url)
